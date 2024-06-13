711acfe522aaae4b61b51d6452533f87668df5ba
Nikita Stoliarov <stlrvnk@gmail.com>
2024-06-13 16:19:08 +0300

    check if audio url exists

--- utils.py
+++ utils.py
@@ -7,16 +7,17 @@ from logging_config import logger
     
 def download_audio(audio_url) -> str | None:
     logger.info(f"DOWNLOAD AUDIO - {audio_url}")
-    response = httpx.get(audio_url)
-    status_code = response.status_code
-    logger.info(f"STATUS CODE - {status_code}")
-    
-    if status_code == 200:
-        audio_data = response.content
-        file_name = f"{int(time.time())}_audio.ogg"
+    if audio_url:
+        response = httpx.get(audio_url)
+        status_code = response.status_code
+        logger.info(f"STATUS CODE - {status_code}")
         
-        with open(file_name, "wb") as file:
-            file.write(audio_data)
-        return file_name
+        if status_code == 200:
+            audio_data = response.content
+            file_name = f"{int(time.time())}_audio.ogg"
+            
+            with open(file_name, "wb") as file:
+                file.write(audio_data)
+            return file_name
             
     
